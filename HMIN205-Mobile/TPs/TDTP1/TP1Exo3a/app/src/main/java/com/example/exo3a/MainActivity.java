70bcadba3f2090579cf031b0948fa599313ac59a
Alex <canton.alex@live.fr>
2021-01-25 21:58:47 +0100

    exo5

--- HMIN205-Mobile/TPs/TDTP1/TP1Exo3a/app/src/main/java/com/example/exo3a/MainActivity.java
+++ HMIN205-Mobile/TPs/TDTP1/TP1Exo3a/app/src/main/java/com/example/exo3a/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.exo3a;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
