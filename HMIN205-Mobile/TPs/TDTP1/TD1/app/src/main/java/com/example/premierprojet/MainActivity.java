b0caaa5f4402c67361e1496cab11b86780f4c1c3
Alex <canton.alex@live.fr>
2021-01-28 13:31:20 +0100

    mis au propre pour le rendu

--- HMIN205-Mobile/TPs/TDTP1/TD1/app/src/main/java/com/example/premierprojet/MainActivity.java
+++ HMIN205-Mobile/TPs/TDTP1/TD1/app/src/main/java/com/example/premierprojet/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -21,46 +22,47 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+// Exo 1 à 4
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button button1 = findViewById(R.id.btn1);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                Toast.makeText(getApplicationContext(), R.string.btn1Text, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Button button2 = findViewById(R.id.btn2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                Toast.makeText(getApplicationContext(), R.string.btn2Text, Toast.LENGTH_SHORT).show();
-
-                new AlertDialog.Builder(MainActivity.this).setTitle(R.string.adTitle)
-                        .setMessage(R.string.adMsg)
-                        .setPositiveButton(R.string.adPositiveButton, null).create().show();
-            }
-        });
-        button2.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                new AlertDialog.Builder(MainActivity.this).setTitle(R.string.adTitle)
-                        .setMessage(R.string.adMsg2)
-                        .setPositiveButton(R.string.adPositiveButton, null).create().show();
-                return true;
-            }
-        });
+//        Button button1 = findViewById(R.id.btn1);
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View arg0) {
+//                Toast.makeText(getApplicationContext(), R.string.btn1Text, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        Button button2 = findViewById(R.id.btn2);
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View arg0) {
+//                Toast.makeText(getApplicationContext(), R.string.btn2Text, Toast.LENGTH_SHORT).show();
+//
+//                new AlertDialog.Builder(MainActivity.this).setTitle(R.string.adTitle)
+//                        .setMessage(R.string.adMsg)
+//                        .setPositiveButton(R.string.adPositiveButton, null).create().show();
+//            }
+//        });
+//        button2.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View v) {
+//                new AlertDialog.Builder(MainActivity.this).setTitle(R.string.adTitle)
+//                        .setMessage(R.string.adMsg2)
+//                        .setPositiveButton(R.string.adPositiveButton, null).create().show();
+//                return true;
+//            }
+//        });
 
 // Exo 1
 //        LinearLayout container = new LinearLayout(this);
 //        setContentView(container);
-
+//
 //        TextView tv = new TextView(this);
 //        tv.setText("Hello Android");
 //        container.addView(tv);
@@ -90,5 +92,45 @@ public class MainActivity extends AppCompatActivity {
 //                displayMsg("bt1");
 //            }
 //        });
+//    }
+//}
+
+// Exo 5
+public class MainActivity extends AppCompatActivity {
+    private CheckBox linux, macos, windows;
+    private Button button;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnChkWindows();
+        addListenerOnButton();
+    }
+    public void addListenerOnChkWindows() {
+        windows = (CheckBox) findViewById(R.id.windows_option);
+        windows.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (((CheckBox) v).isChecked()) {
+                    Toast.makeText(MainActivity.this,"Bro, try Linux :)", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    public void addListenerOnButton() {
+        linux = (CheckBox) findViewById(R.id.linux_option);
+        macos = (CheckBox) findViewById(R.id.macos_option);
+        windows = (CheckBox) findViewById(R.id.windows_option);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuffer result = new StringBuffer();
+                result.append("Linux check : ").append(linux.isChecked());
+                result.append("\nMac OS check : ").append(macos.isChecked());
+                result.append("\nWindows check :").append(windows.isChecked());
+                Toast.makeText(MainActivity.this, result.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
