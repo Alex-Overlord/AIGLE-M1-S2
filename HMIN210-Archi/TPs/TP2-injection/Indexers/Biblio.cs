5412bf46b4920195dcebcbe2b8fa21650f141338
Alex <canton.alex@live.fr>
2021-02-08 17:40:53 +0100

    td C#

---
+++ HMIN210-Archi/TPs/TP2-injection/Indexers/Biblio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Indexers {
+
+    class Biblio<T> {
+        private T[] catalogue = new T[1000];
+
+        public T this[int isbn] {
+            get => catalogue[isbn];
+            set => catalogue[isbn] = value;
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Programme de test de la classe Biblio.");
+
+            var biblio = new Biblio<string>();
+            biblio[042] = "L'histoire sans fin";
+            biblio[007] = "Harry Potter";
+            biblio[068] = "Le meilleur des Mondes";
+            biblio[911] = "Dune";
+
+            for (int i = 0; i < 1000; i++) {
+                if (biblio[i] != null) {
+                    Console.WriteLine(biblio[i]);
+                }
+            }
+        }
+    }
+}
---
+++ HMIN210-Archi/TPs/TP2-injection/Operators/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Operators {
+    class Program {
+
+        public class Complexe {
+            public double x;
+            public double y;
+            public double i;
+            public Complexe(double x, double y, double i) {
+                this.x = x;
+                this.y = y;
+                this.i = i;
+            }
+
+            public static Complexe operator +(Complexe c1, Complexe c2) {
+                return new Complexe(c1.x + c2.x, c1.y + c2.y, c1.i + c2.i);
+            }
+            public static Complexe operator -(Complexe c1, Complexe c2) {
+                return new Complexe(c1.x - c2.x, c1.y - c2.y, c1.i - c2.i);
+            }
+            public static Complexe operator *(Complexe c1, Complexe c2) {
+                return new Complexe(c1.x * c2.x, c1.y * c2.y, c1.i * c2.i);
+            }
+
+            public override string ToString() {
+                return "(" + x + ", " + y + ", " + i + ")";
+            }
+        }
+
+        static void Main(string[] args) {
+            Console.WriteLine("Programme de test des Opérateurs en C#.\n");
+
+            Complexe c1 = new Complexe(1, 2, 0);
+            Complexe c2 = new Complexe(5, -3, 2);
+            Complexe c3 = new Complexe(-6, 1, -1);
+
+            Console.WriteLine("c1 + c2 = {0}", c1 + c2);
+            Console.WriteLine("c2 - c3 = {0}", c2 - c3);
+            Console.WriteLine("c3 * c2 = {0}", c3 * c2);
+        }
+    }
+}
