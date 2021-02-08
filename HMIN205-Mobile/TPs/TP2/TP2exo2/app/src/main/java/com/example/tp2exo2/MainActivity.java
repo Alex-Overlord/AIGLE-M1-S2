54844c5df721b0647443fb01a4291cd01b6a0ad3
Alex <canton.alex@live.fr>
2021-02-08 21:55:23 +0100

    tp2 android exo 2 et 3

--- HMIN205-Mobile/TPs/TP2/TP2exo2/app/src/main/java/com/example/tp2exo2/MainActivity.java
+++ HMIN205-Mobile/TPs/TP2/TP2exo2/app/src/main/java/com/example/tp2exo2/MainActivity.java
@@ -2,24 +2,28 @@ package com.example.tp2exo2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        Sensor defaultProximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        TextView tv = findViewById(R.id.tv);
 
-        Log.v("SensorActivity", "defaultProximitySensor = " + defaultProximitySensor.getName());
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
+            tv.setText(sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD).toString());
+        } else {
+            tv.setText(R.string.sensor_unavailable);
+        }
     }
 }
