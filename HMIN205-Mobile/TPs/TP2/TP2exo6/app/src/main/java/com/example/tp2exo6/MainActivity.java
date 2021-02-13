e317b7dcd1069573608f780518627c9ebd26ff1f
Alex <canton.alex@live.fr>
2021-02-13 16:05:16 +0100

    android tp2exo6 vraiment funi

--- HMIN205-Mobile/TPs/TP2/TP2exo6/app/src/main/java/com/example/tp2exo6/MainActivity.java
+++ HMIN205-Mobile/TPs/TP2/TP2exo6/app/src/main/java/com/example/tp2exo6/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.tp2exo6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -16,8 +19,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     Sensor proximity;
     TextView tv0;
     TextView tv1;
-    TextView tv2;
-    TextView tv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +30,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         tv0 = findViewById(R.id.tv0);
         tv1 = findViewById(R.id.tv1);
-        tv2 = findViewById(R.id.tv2);
-        tv3 = findViewById(R.id.tv3);
 
         boolean supported = sensorManager.registerListener(this,
                 proximity,
@@ -44,48 +43,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
             onProximityChanged(event);
         }
     }
 
     private void onProximityChanged(SensorEvent event) {
-        int red = Color.rgb(255, 0, 0);
-        int green = Color.rgb(0, 255, 0);
-        int black = Color.rgb(0, 0, 0);
 
-        float x = event.values[0]; // vitesse angulaire autour de x
-        float y = event.values[1]; // vitesse angulaire autour de y
-        float z = event.values[2]; // vitesse angulaire autour de z
-
-        tv1.setText("x = " + (int) x);
-        tv2.setText("y = " + (int) y);
-        tv3.setText("z = " + (int) z);
-
-        if (x < -1) {
-            tv1.setTextColor(red);
-        } else if (x > 1) {
-            tv1.setTextColor(green);
+        if (event.values[0] < proximity.getMaximumRange()) {
+            getWindow().getDecorView().setBackgroundColor(Color.RED);
         } else {
-            tv1.setTextColor(black);
+            getWindow().getDecorView().setBackgroundColor(Color.GREEN);
         }
 
-        if (y < -1) {
-            tv2.setTextColor(red);
-        } else if (y > 1) {
-            tv2.setTextColor(green);
-        } else {
-            tv2.setTextColor(black);
-        }
-
-        if (z < -1) {
-            tv3.setTextColor(red);
-        } else if (z > 1) {
-            tv3.setTextColor(green);
-        } else {
-            tv3.setTextColor(black);
-        }
-
+        tv1.setText(event.values[0] + " cm");
     }
 
     @Override
