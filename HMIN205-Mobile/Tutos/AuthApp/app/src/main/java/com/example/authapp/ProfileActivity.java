72e906b39e7447171d6865fa7228469100962a32
Alex <canton.alex@live.fr>
2021-04-19 16:34:23 +0200

    update

--- HMIN205-Mobile/Tutos/AuthApp/app/src/main/java/com/example/authapp/ProfileActivity.java
+++ HMIN205-Mobile/Tutos/AuthApp/app/src/main/java/com/example/authapp/ProfileActivity.java
@@ -1,14 +1,27 @@
 package com.example.authapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
 
     private Button logout;
 
@@ -21,6 +34,38 @@ public class ProfileActivity extends AppCompatActivity {
         logout.setOnClickListener(v -> {
             FirebaseAuth.getInstance().signOut();
             startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+        });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView greetingTextView = findViewById(R.id.greeting);
+        final TextView fullNameTextView = findViewById(R.id.fullName);
+        final TextView emailTextView = findViewById(R.id.emailAddress);
+        final TextView ageTextView = findViewById(R.id.age);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if (userProfile != null) {
+                    String fullName = userProfile.fullName;
+                    String email = userProfile.email;
+                    String age = userProfile.age;
+
+                    greetingTextView.setText("Welcome, " + fullName + "!");
+                    fullNameTextView.setText(fullName);
+                    emailTextView.setText(email);
+                    ageTextView.setText(age);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfileActivity.this, "Something wrong happened!", Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
